f9594be1d88abd2cbc941f4c71425cd28d1f4be7
zhourui <923291595@qq.com>
2021-08-09 17:53:29 +0800

    订单支付：服务生产者：01

---
+++ springcloud/cloud-provider-payment8001/src/main/java/org/example/springcloud/dao/PaymentDao.java
@@ -0,0 +1,14 @@
+package org.example.springcloud.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.example.springcloud.entities.Payment;
+import org.springframework.stereotype.Repository;
+
+/*@Mapper*/
+@Repository
+public interface PaymentDao {
+
+    public int create(Payment payment);
+
+    public Payment get(@Param("id") Long id);
+}
